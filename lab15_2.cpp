f2479b8226c42e7c98c046bdd1484355aea4e838
werasak mayer <jokerwwe0542@gmail.com>
2023-01-17 15:58:57 +0700

    "complete"

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,35 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void showMatrix(const bool a[][N]){
+    	for(int i=0;i<N;i++){ 
+		    for(int j=0;j<N;j++){
+			    cout << a[i][j] ;
+			    if(j!=N-1) cout << " ";
+		    }
+		cout << endl;
+	}
+}
+
+void inputMatrix(double a[][N]){
+	for(int i=0;i<N;i++){
+		cout << "Row "<< i+1 <<": ";
+		for(int j=0;j<N;j++){
+		    cin >> a[i][j] ;
+		}
+	}
+}
+
+void findLocalMax(const double a[][N], bool b[][N]){
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			if(i==0||i==N-1||j==0||j==N-1){
+				b[i][j] =0;
+			}
+			else if(a[i][j]>= a[i-1][j] && a[i][j]>= a[i+1][j] && a[i][j]>= a[i][j+1] && a[i][j]>= a[i][j-1]){
+				b[i][j]=1;
+			}else b[i][j] =0;
+		}
+	}
+}
