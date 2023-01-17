3eff8681b793fca2310fc5229e999f4c96c08a14
werasak mayer <jokerwwe0542@gmail.com>
2023-01-17 15:09:55 +0700

    "O JO"

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,8 +3,23 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+    for(int i=1;i<N;i++){
+        cout << "Pass " << i << ":";
+    int j= i-1;
+    T key = d[i];
+        while(d[j]<key && j>=0){
+            d[j+1]=d[j];
+            j--;
+        }
+        d[j+1]=key;
+                for(int j=0;j < N;j++){
+			        cout << d[j] ;
+			    if(j!=N-1) cout << " ";
+		}
+		cout << endl; 
+	}
 }
+    
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
